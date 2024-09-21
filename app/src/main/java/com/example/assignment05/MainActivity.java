9b133f57e991c0b01e517092228330cd39487805
Colby Lalicker <colby.d.lalicker@gmail.com>
2024-09-21 13:00:21 -0400

    user class, welcome page

--- app/src/main/java/com/example/assignment05/MainActivity.java
+++ app/src/main/java/com/example/assignment05/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.assignment05;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,18 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        start = findViewById(R.id.start_button);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent userInfo = new Intent(MainActivity.this, UserInfo.class);
+                startActivity(userInfo);
+                finish();
+            }
+        });
+
+
     }
 }
