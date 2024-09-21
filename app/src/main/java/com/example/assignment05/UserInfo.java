642e272a660b2e2665ab3e32825a16b5f3455b2d
requiem <z.nelson13@gmail.com>
2024-09-21 14:42:09 -0400

    Finished profile intents

--- app/src/main/java/com/example/assignment05/UserInfo.java
+++ app/src/main/java/com/example/assignment05/UserInfo.java
@@ -2,6 +2,7 @@ package com.example.assignment05;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,6 +11,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -22,8 +27,22 @@ public class UserInfo extends AppCompatActivity {
     EditText age_edit_userinfo;
 
     TextView country_display_userinfo;
+    TextView dob_display_userinfo;
 
-    User user;
+    User user = new User();
+
+    ActivityResultLauncher<Intent> startDOBActivity = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            dob_display_userinfo = findViewById(R.id.dob_display_userinfo);
+
+            if ( result.getResultCode() == RESULT_OK && result.getData() != null ) {
+                Log.d("DOB", result.getData().getStringExtra("DOB"));
+                user.dob = result.getData().getStringExtra("DOB");
+                dob_display_userinfo.setText(user.dob);
+            }
+        }
+    });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,10 +98,18 @@ public class UserInfo extends AppCompatActivity {
             }
         });
 
+        findViewById(R.id.selectDoB_userinfo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(UserInfo.this, DateOfBirth.class);
+
+                startDOBActivity.launch(intent);
+            }
+        });
+
         findViewById(R.id.submit_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                user = new User();
                 if ( String.valueOf(name_edit_userinfo.getText()).isEmpty() ) {
                     Toast.makeText(UserInfo.this, "Please enter a username", Toast.LENGTH_SHORT).show();
                 } else if ( String.valueOf(email_edit_userinfo.getText()).isEmpty() ) {
@@ -91,13 +118,21 @@ public class UserInfo extends AppCompatActivity {
                     Toast.makeText(UserInfo.this, "Please enter an age", Toast.LENGTH_SHORT).show();
                 } else if ( userCountry[0] == null ) {
                     Toast.makeText(UserInfo.this, "Please select a country", Toast.LENGTH_SHORT).show();
+                } else if ( user.dob.isEmpty() ) {
+                    Toast.makeText(UserInfo.this, "Please select your Date of Birth", Toast.LENGTH_SHORT).show();
                 } else {
                     user.name = String.valueOf(name_edit_userinfo.getText());
                     user.email = String.valueOf(email_edit_userinfo.getText());
                     user.age = Integer.parseInt(String.valueOf(age_edit_userinfo.getText()));
                     user.country = userCountry[0];
 
-                    Log.d("SUBMIT", "User Created: " + user.name + ", " + user.email + ", " + String.valueOf(user.age) + ", " + user.country);
+                    Log.d("SUBMIT", "User Created: " + user.name + ", " + user.email + ", " + String.valueOf(user.age) + ", " + user.country + ", " + user.dob);
+
+                    Intent toProfile = new Intent(UserInfo.this, Profile.class);
+
+                    toProfile.putExtra("USER", user);
+
+                    startActivity(toProfile);
                 }
             }
         });
